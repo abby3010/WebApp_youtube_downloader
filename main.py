2b46ce8b456f01157fbeb8b7c3d9d5d3f9f60db9
abby3010 <abhayubhale.30@gmail.com>
2020-07-20 10:25:03 +0530

    hosting the site

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import pytube
 from pytube import YouTube
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def main():
-    if request.method == "POST":
-        url = request.form["url"]
-        try:
-            yt = YouTube(url)
-            yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-            return render_template("index.html", eval=0)
-        except Exception as e:
-            return render_template("index.html", error_msg=e , eval=1)
+    url = request.form.get("url")
+    try:
+        yt = YouTube(url)
+        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+        return render_template("index.html", eval=0)
+    except Exception as e:
+        print(e)    
+        return render_template("index.html", eval=1)
     return render_template("index.html")
 
 if __name__ == '__main__':
