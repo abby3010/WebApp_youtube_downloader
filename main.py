cb12004ee41eb28e1d5bc31cb8a9dd05beb0b57f
abby3010 <abhayubhale.30@gmail.com>
2020-07-20 10:33:42 +0530

    fixed bug

--- main.py
+++ main.py
@@ -7,13 +7,14 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def main():
     url = request.form.get("url")
-    try:
-        yt = YouTube(url)
-        yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-        return render_template("index.html", eval=0)
-    except Exception as e:
-        print(e)    
-        return render_template("index.html", eval=1)
+    if request.method == "POST":
+        try:
+            yt = YouTube(url)
+            yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+            return render_template("index.html", eval=0)
+        except Exception as e:
+            print(e)    
+            return render_template("index.html", eval=1)
     return render_template("index.html")
 
 if __name__ == '__main__':
